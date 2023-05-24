2d8257e024b2cbf44bec5b5c4ce85f1446c6cdf4
Hwangkyeonghun <100209625+Hwangkyeonghun@users.noreply.github.com>
2023-05-24 17:59:41 +0900

    0524

--- examples/src/ch08/Car.java
+++ examples/src/ch08/Car.java
@@ -2,10 +2,20 @@ package ch08;
 
 public class Car {
 	private String name;
+	
+	public Car() {}
 	public Car(String name) {
 		this.name=name;
 	}
 	public String getName() {
 		return name;
+	}
+	
+	
+	Tire t1=new HankookTire();
+	Tire t2=new HankookTire();
+	void run() {
+		t1.roll();
+		t2.roll();
 	}
 }
